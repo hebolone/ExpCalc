c9760fd3043980daf2b5cf3558a24682a6346e93
hebolone <polo.simone@gmail.com>
2017-10-01 00:09:59 +0200

    Initial commit

---
+++ src/Exp/CExp.kt
@@ -0,0 +1,81 @@
+package Exp
+
+import java.lang.Math.pow
+
+open class CExp(iStartingLevel : Int) {
+    //  *** PROPERTIES ***
+    val Exp : Int
+        get() = m_Exp
+    val Level : Int
+        get() = m_CurrentLevel
+    val ExpToNextLevel
+        get() = m_FuncExp(m_CurrentLevel + 1) - m_Exp
+    //  *** MEMBERS ***
+    protected var m_FuncExp : (Int) -> Int = { m_FuncPow(it, 2) + m_FuncPow(3 * (it - 1), 2) - 1 }
+    private val m_FuncPow : (Int, Int) -> Int = { base, exp -> pow(base.toDouble(), exp.toDouble()).toInt() }
+    protected var m_CurrentLevel : Int = iStartingLevel
+    protected var m_Exp : Int = m_FuncExp(m_CurrentLevel)
+    private val MAX_LEVEL = 9999
+    protected val MAX_EXP = m_FuncExp(MAX_LEVEL)
+    //  *** METHODS ***
+    init {
+        if(iStartingLevel < 1 || iStartingLevel > MAX_LEVEL)
+            throw IllegalArgumentException("Bad starting level, it has to be between 1 and $MAX_LEVEL")
+        m_CurrentLevel = iStartingLevel
+        m_Exp = m_FuncExp(m_CurrentLevel)
+    }
+    fun AddExp(iAmount : Int) : Pair<Int, Int> {
+        //  Return levels gained and current level
+        //  Add exp
+        m_Exp += iAmount
+        if(m_Exp >= MAX_EXP)
+            m_Exp = MAX_EXP
+        //  Check if level changed
+        var levelsCounter : Int = m_CurrentLevel + 1
+        var nextLevelExpNeeded = m_FuncExp(levelsCounter)
+        while(nextLevelExpNeeded <= m_Exp) {
+            nextLevelExpNeeded = m_FuncExp(++ levelsCounter)
+        }
+        val levelsGained = -- levelsCounter - m_CurrentLevel
+        m_CurrentLevel += levelsGained
+        return Pair(levelsGained, m_CurrentLevel)
+    }
+    fun SetFuncExp(iFunc : (Int) -> Int) {
+        m_FuncExp = iFunc
+    }
+}
+//-------------------------------------------------------------------------------
+class CExpProgressive(iStartingLevel : Int) : CExp(iStartingLevel) {
+    //  *** EVENTS ***
+    private val eventhandler_onLevelGained = EventHandler<Int>()
+    val onLevelGained = Event(eventhandler_onLevelGained)
+    private val eventhandler_onExpGained = EventHandler<Int>()
+    val onExpGained = Event(eventhandler_onExpGained)
+    //  *** METHODS ***
+    fun AddExpProgression(iAmount : Int) {
+        var counter = 0
+        while(counter < iAmount) {
+            counter ++
+            m_Exp ++
+            LaunchOnExpGained(m_Exp)
+            if(m_Exp >= MAX_EXP) {
+                m_Exp = MAX_EXP
+                return
+            }
+            //  Check if level changed
+            val nextLevelExpNeeded = m_FuncExp(m_CurrentLevel + 1)
+            if(m_Exp == nextLevelExpNeeded) {
+                //  Increment level
+                m_CurrentLevel ++
+                //  Launch a message
+                LaunchOnLevelGained(m_CurrentLevel)
+            }
+        }
+    }
+    private fun LaunchOnLevelGained(iCurrentLevel : Int) {
+        eventhandler_onLevelGained(iCurrentLevel)
+    }
+    private fun LaunchOnExpGained(iCurrentExp : Int) {
+        eventhandler_onExpGained(iCurrentExp)
+    }
+}
---
+++ src/main.kt
@@ -0,0 +1,33 @@
+import Exp.*
+
+fun main(args : Array<String>) {
+    /*val exp = CExp(1)
+    println("Starting from level ${exp.Level} (exp point: ${exp.Exp}, exp to next level: ${exp.ExpToNextLevel})")
+    print("Insert amount of exp: ")
+    var userInput = readLine()!!
+    while(userInput != "") {
+        val (levelsGained, currentLevel) = exp.AddExp(userInput.toInt())
+        println("Current exp: ${exp.Exp}, current level: $currentLevel, levels gained: $levelsGained, exp to next level: ${exp.ExpToNextLevel}")
+        userInput = readLine()!!
+    }*/
+
+    val exp = CExpProgressive(1)
+    //exp.SetFuncExp { it + 4 }
+    exp.onLevelGained += { OnMessageLevelGained(it) }
+    //exp.onExpGained += { OnMessageExpGained(it) }
+    println("Starting from level ${exp.Level} (exp point: ${exp.Exp}, exp to next level: ${exp.ExpToNextLevel})")
+    print("Insert amount of exp: ")
+    var userInput = readLine()!!
+    while(userInput != "") {
+        exp.AddExpProgression(userInput.toInt())
+        println("Current exp is: ${exp.Exp}, exp to next level: ${exp.ExpToNextLevel}")
+        userInput = readLine()!!
+    }
+}
+
+fun OnMessageLevelGained(iMessage : Int) {
+    println("Level gained, current level is: $iMessage")
+}
+fun OnMessageExpGained(iMessage : Int) {
+    println("Exp gained, current exp is: $iMessage")
+}
